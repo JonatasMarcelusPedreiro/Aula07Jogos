215cda87faf0ee7865ff9ebec9766b6304578dae
JonatasMarcelusPedreiro <135742926+JonatasMarcelusPedreiro@users.noreply.github.com>
2023-06-13 17:16:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07Jogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random sorteador = new Random();
+
+            int numero;
+            int sorteio = sorteador.Next(1, 11);
+
+            Console.WriteLine(" Bem-vindos a conraditos Jogos");
+            Console.WriteLine(" Adivinhe o número de 1 até 10");
+            Console.WriteLine(" Digite o número");
+            numero = Int32.Parse (Console.ReadLine());
+
+
+            
+
+
+
+           
+             if ( numero == sorteio)
+            {
+                Console.WriteLine(" Parabéns, você acertou o número e ganhou!");
+
+
+
+
+            }else
+            {
+
+                Console.WriteLine(" Que pena, você errou! Tente novamente...");
+                
+                return;
+
+
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
